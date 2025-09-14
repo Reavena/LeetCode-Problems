78dffa60743a2807cbcc481a0fb6efc98767374c
Reavena <145583065+Reavena@users.noreply.github.com>
2025-09-14 20:16:30 +0200

    Create TwoSum.py

---
+++ 1.Two Sum/TwoSum.py
@@ -0,0 +1,28 @@
+class SolutionBF(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range (len(nums)):
+            for j in range (1, len(nums)):
+                if nums[j] ==  target - nums[i]:
+                    return [i,j]
+        return []
+
+class SolutionTwoPass(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash = {}
+        for i in range (len(nums)):
+            hash[nums[i]] = i
+        for j in range (len(nums)):
+            comp = target - nums[j]
+            if comp in hash and hash[comp]!=j:
+                return [j,hash[comp]]
+        return []
